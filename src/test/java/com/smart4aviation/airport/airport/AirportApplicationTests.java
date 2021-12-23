22c03646fd4959472d1893a757984c7e069d1380
grzegorz <konopkagrzeg@gmail.com>
2021-12-23 23:58:43 +0100

    init commit

---
+++ src/test/java/com/smart4aviation/airport/airport/AirportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart4aviation.airport.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
