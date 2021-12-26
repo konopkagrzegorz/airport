2e0c614893b4fea37e46410276137f4b5c7aabaf
grzegorz <konopkagrzeg@gmail.com>
2021-12-26 20:10:40 +0100

    added getFlight functionality

--- src/main/java/com/smart4aviation/aviation/airport/controller/FlightController.java
+++ src/main/java/com/smart4aviation/aviation/airport/controller/FlightController.java
@@ -2,8 +2,12 @@ package com.smart4aviation.aviation.airport.controller;
 
 import com.smart4aviation.aviation.airport.domain.Flight;
 import com.smart4aviation.aviation.airport.repository.FlightRepository;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +24,12 @@ public class FlightController {
     @GetMapping("flights/all")
     public ResponseEntity<List<Flight>> getAllFlight() {
         return ResponseEntity.ok(flightRepository.findAll());
+    }
+
+    @GetMapping("flights")
+    public ResponseEntity<Flight> getFlightDetails(@RequestParam int flightNumber, @RequestParam String date) {
+        String pattern = "yyyy-MM-dd'T'HH:mm:ssZ";
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(pattern);
+        return ResponseEntity.ok(flightRepository.findFlightByFlightNumberAndDate(flightNumber, formatter.parseDateTime(date)));
     }
 }
--- src/main/java/com/smart4aviation/aviation/airport/repository/FlightRepository.java
+++ src/main/java/com/smart4aviation/aviation/airport/repository/FlightRepository.java
@@ -1,10 +1,14 @@
 package com.smart4aviation.aviation.airport.repository;
 
 import com.smart4aviation.aviation.airport.domain.Flight;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Repository
 public class FlightRepository {
@@ -17,5 +21,16 @@ public class FlightRepository {
 
     public List<Flight> findAll() {
         return flights;
+    }
+
+    public Flight findFlightByFlightNumberAndDate(int flightId, DateTime date) {
+        Optional<Flight> flight = flights.stream().filter(getFlight(flightId,date)).findFirst();
+        if (flight.isEmpty())
+            throw new NoSuchElementException(String.format("Flight with flight id: %d",flightId));
+        return flight.get();
+    }
+
+    private Predicate<Flight> getFlight(int flightNumber, DateTime date) {
+        return flight -> flight.getFlightNumber() == flightNumber && flight.getDepartureDate().equals(date);
     }
 }
