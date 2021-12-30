f0686544df9012fdd15c33b665a24945b117faf4
grzegorz <konopkagrzeg@gmail.com>
2021-12-30 22:18:10 +0100

    fixed wrong unit test

--- src/test/java/com/smart4aviation/aviation/airport/mapper/FlightEntityMapperTest.java
+++ src/test/java/com/smart4aviation/aviation/airport/mapper/FlightEntityMapperTest.java
@@ -4,9 +4,10 @@ import com.smart4aviation.aviation.airport.domain.FlightEntity;
 import com.smart4aviation.aviation.airport.domain.IATA;
 import com.smart4aviation.aviation.airport.json.FlightEntityJSON;
 import org.joda.time.DateTime;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FlightEntityMapperTest {
 
@@ -26,15 +27,13 @@ class FlightEntityMapperTest {
                 "MIT",
                 "2017-08-28T07:57:33-02:00");
 
-        FlightEntity flightEntity = new FlightEntity(
+        FlightEntity expected = new FlightEntity(
                 0,
                 4229,new IATA("LAX"),
                 new IATA("MIT"),
                 new DateTime("2017-08-28T09:57:33.000Z"));
 
-        Assertions.assertEquals(flightEntity.getFlightId(),flightEntityJSON.getFlightId());
-        Assertions.assertEquals(flightEntity.getFlightNumber(),flightEntityJSON.getFlightNumber());
-        Assertions.assertEquals(flightEntity.getDepartureAirportIATACode().toString(),flightEntityJSON.getDepartureAirportIATACode());
-        Assertions.assertEquals(flightEntity.getArrivalAirportIATACode().toString(),flightEntityJSON.getArrivalAirportIATACode());
+        FlightEntity actual = flightEntityMapper.flightEntityJSONtoFlightMapper(flightEntityJSON);
+        assertEquals(expected,actual);
     }
 }
